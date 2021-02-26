00c1fe5b126e34df8010cf8ab78434f540d61a99
Luziberto <luzibertomendesjunior@hotmail.com>
2021-02-26 09:05:33 -0300

    second commit

---
+++ app/Models/Tweet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+
+    protected $fillabel = ['content'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Curso Laravel Livewire</title>
+    @livewireStyles
 </head>
 <body>
-
+    <div class="container">
+        {{ $slot }}
+    </div>
+    @livewireScripts
 </body>
 </html>
